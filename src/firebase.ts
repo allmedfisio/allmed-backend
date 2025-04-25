239cee1b8b1618a8b9164ce62edcfa7453f4fe6e
Paolo Zuccaro <zuccaro.paolo@gmail.com>
2025-04-25 13:23:42 +0200

    Modificata inizializzazione privateKey

--- src/firebase.ts
+++ src/firebase.ts
@@ -14,7 +14,7 @@ if (getApps().length === 0) {
     credential: cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+      privateKey: process.env.FIREBASE_PRIVATE_KEY,
     }),
   });
   console.log("✅ Firebase inizializzato con successo!");
